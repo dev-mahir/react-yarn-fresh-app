59924a2aadb251af19ad65b653ca253084fd9d1d
dev-mahir <mdmahir.ds@gmail.com>
2023-01-06 14:57:04 +0600

    change structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home";
+
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,9 @@
 import React from 'react'
-import Banner from '../components/Banner/Banner'
-import WhatIDo from '../components/WhatIDo/WhatIDo'
-import About from '../components/About/About'
-import Portfolio from '../components/Portfolio/Portfolio'
-import Testimonial from '../components/Testimonial/Testimonial'
-
 
 const Home = () => {
-    return (
-        <>
-            <Banner />
-            <WhatIDo />
-            <About />
-            <Portfolio />
-            <Testimonial/>
-        </>
-    )
+  return (
+    <div>Home</div>
+  )
 }
 
 export default Home
